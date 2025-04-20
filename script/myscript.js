eb02eda461641cfb927f49c2e3e694c2bdbcddee
akshat <akshatsharda18@gmail.com>
2025-04-20 18:29:46 +0530

    1 Add js to handle popup, 2 Add basic css for it, 3 Add placeholder info

---
+++ script/myscript.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById("modalInfo");
+
+function showModal() {
+    modal.classList.add('active');
+}
+
+function hideModal() {
+    modal.classList.remove('active');
+}
